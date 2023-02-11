dae0eeba7199276a8950fc252516195d7bd8d4d3
Greko <gyan2020cs093@abesit.edu.in>
2023-02-11 13:54:11 +0530

    popup added

---
+++ assets/js/popup.js
@@ -0,0 +1,29 @@
+window.addEventListener("load", function(){
+    setTimeout(
+        function open(event){
+            document.querySelector(".popup").style.display = "block";
+        },
+        2000 
+    )
+});
+
+
+document.querySelector("#close").addEventListener("click", function(){
+    document.querySelector(".popup").style.display = "none";
+});
+// SPONSOR SLIDER
+
+function prev(){
+    document.getElementById('slider-container').scrollLeft -= 270;
+}
+
+function next()
+{
+    document.getElementById('slider-container').scrollLeft += 270;
+}
+
+
+$(".slide img").on("click" , function(){
+$(this).toggleClass('zoomed');
+$(".overlay").toggleClass('active');
+})
