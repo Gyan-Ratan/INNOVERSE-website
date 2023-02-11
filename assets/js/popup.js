d40fc75f5002b7f43ce32baef069d7d806d7f37a
Greko <gyan2020cs093@abesit.edu.in>
2023-02-11 14:51:36 +0530

    .

--- assets/js/popup.js
+++ assets/js/popup.js
@@ -11,19 +11,3 @@ window.addEventListener("load", function(){
 document.querySelector("#close").addEventListener("click", function(){
     document.querySelector(".popup").style.display = "none";
 });
-// SPONSOR SLIDER
-
-function prev(){
-    document.getElementById('slider-container').scrollLeft -= 270;
-}
-
-function next()
-{
-    document.getElementById('slider-container').scrollLeft += 270;
-}
-
-
-$(".slide img").on("click" , function(){
-$(this).toggleClass('zoomed');
-$(".overlay").toggleClass('active');
-})
